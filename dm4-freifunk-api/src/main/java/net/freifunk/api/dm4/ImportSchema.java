2a5ad898b3f97d1cfd2011a3733a23a160cb479d
mt <martin.tippmann@gmail.com>
2014-09-15 22:28:05 +0200

    try to parse data..

--- dm4-freifunk-api/src/main/java/net/freifunk/api/dm4/ImportSchema.java
+++ dm4-freifunk-api/src/main/java/net/freifunk/api/dm4/ImportSchema.java
@@ -2,6 +2,7 @@ package net.freifunk.api.dm4;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -20,6 +21,9 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import de.deepamehta.core.Topic;
+import de.deepamehta.core.TopicType;
+import de.deepamehta.core.model.TopicModel;
 import de.deepamehta.core.model.TopicTypeModel;
 
 public class ImportSchema {
@@ -32,9 +36,12 @@ public class ImportSchema {
 	private final JsonNodeFactory factory = JsonNodeFactory.instance;
 
 	ImportSchema() throws JsonParseException, IOException {
-		jn = m.readTree(new File("/home/riso3860/spec.json"));
-
+		//jn = m.readTree(new File("/home/riso3860/spec.json"));
+		jn = m.readTree(new File("/home/riso3860/ffSummarizedDir.json"));
 	}
+	
+
+
 
 	Stack<String> type = new Stack<String>();
 
@@ -65,18 +72,25 @@ public class ImportSchema {
 
 	public String printAll(JsonNode node, String f) throws JsonProcessingException, IOException, JSONException {
 		System.out.println("---");
-		//StringBuilder json = new StringBuilder();
 		ObjectNode json = factory.objectNode();
-		type.push(f);
-		//json.append("{\n");
+		
+		if (!f.equals("properties"))
+			type.push(f);
+		
 		Iterator<String> fieldNames = node.fieldNames();
+		
+		Enumeration<String> e = type.elements();
+		while (e.hasMoreElements())
+			e.nextElement();
+		
 		String uri = type.toString().replaceAll(", ", ".")
 				.replaceFirst(".schema.", ".community.")
 				.replaceAll("[\\[\\]]", "");
 		json.put("uri", uri);
-		//json.append("\"uri\": " + "\"" + uri + "\",\n");
-		//json.append("\"data_type_uri\": \"dm4.core.text\",\n");
-		//json.append("\"index_mode_uris\": [\"dm4.core.fulltext\", \"dm4.core.fulltext_key\"],\n");
+		ArrayNode index_mode = factory.arrayNode();
+		index_mode.add("dm4.core.fulltext");
+		index_mode.add("dm4.core.fulltext_key");
+		json.set("index_mode_uris", index_mode);
 		json.put("data_type_uri", "dm.core.text");
 		ArrayNode array = factory.arrayNode();
 		while (fieldNames.hasNext()) {
@@ -89,7 +103,7 @@ public class ImportSchema {
 				ObjectNode assoc = factory.objectNode();
 				assoc.put("child_type_uri", uri+"."+printAll(fieldValue, fieldName).replaceAll("\n",""));
 				assoc.put("child_cardinality_uri", "dm4.core.one");
-				assoc.put("child_cardinality_uri", "dm4.core.one");
+				assoc.put("parent_cardinality_uri", "dm4.core.one");
 				assoc.put("assoc_type_uri", "dm4.core.composition_def");
 				//json.append(("composite: "+uri+"."+printAll(fieldValue, fieldName)).replaceAll("\n", "")+"\n");
 				array.add(assoc);
@@ -113,22 +127,58 @@ public class ImportSchema {
 		TopicTypeModel t = new TopicTypeModel(new JSONObject(json.toString()));
 		System.err.println(t.getUri());
 		System.err.println(t.getAssocDefs());
-		return type.pop();
-
+		if (!f.equals("properties")) {
+				return type.pop();
+		} else return "";
 	}
 
+	public String parseData(JsonNode node, String f) throws JsonProcessingException, IOException, JSONException {
+		System.out.println("---");
+		ObjectNode json = factory.objectNode();
+
+		type.push(f);
+		
+		Iterator<String> fieldNames = node.fieldNames();
+			
+		String uri = type.toString().replaceAll(", ", ".")
+				.replaceAll("[\\[\\]]", "");
+		json.put("type_uri", uri);
+		ObjectNode object = factory.objectNode();
+		while (fieldNames.hasNext()) {
+			String fieldName = fieldNames.next();
+			JsonNode fieldValue = node.get(fieldName);
+			//if (node.findParent("description").get("description") != null)
+			//json.set("value", node.findParent("description").get("description"));
+			
+			if (fieldValue.isObject()) {
+				ObjectNode assoc = factory.objectNode();
+				assoc.set(uri+"."+parseData(fieldValue, fieldName).replaceAll("\n",""),fieldValue);
+				json.set("composite", assoc);
+			} else {
+				json.set(fieldName, fieldValue);
+			}
+		}
+		//json.append("}");
+		System.out.println("xxx");
+		System.out.println(m.writerWithDefaultPrettyPrinter().writeValueAsString(json));
+		TopicModel t = new TopicModel(new JSONObject(json.toString()));
+		System.err.println(t.getUri());
+		System.err.println(t.getCompositeValueModel());
+		
+				return type.pop();
+	}
+	
 	public static void main(String[] args) throws JsonProcessingException,
 			IOException, JSONException {
 
 		ImportSchema test = new ImportSchema();
 
 		// test.parseSchema(test.jn, "start");
+		
 
-		try {
-			test.printAll(test.jn, "freifunk");
-		} catch (NullPointerException e) {
-			System.err.println(e);
-		}
+
+			test.parseData(test.jn.get("ansbach"), "freifunk");
+	
 
 		// for (Entry<String, ?> e : test.types.entrySet()) {
 		// String uri = e.getKey().replaceAll(", ",
@@ -151,125 +201,6 @@ public class ImportSchema {
 		//
 		// System.err.println(t.getUri());
 
-		/**
-		 * output [freifunk.community.state.focus] : [title, type, description,
-		 * required, items] [freifunk.community.nodeMaps.items.technicalType] :
-		 * [title, type, description, enum, default, required]
-		 * [freifunk.community.api] : [title, type, description, enum, default,
-		 * required] [freifunk.community.location.address.Name] : [title, type,
-		 * description, required] [freifunk.community.contact.twitter] : [title,
-		 * type, pattern, description, required]
-		 * [freifunk.community.techDetails.networks] : []
-		 * [freifunk.community.feeds.items.type] : [title, type, description,
-		 * required] [freifunk.community.state.focus.items] : [type, title,
-		 * enum] [freifunk.community.techDetails.updatemode.items] : [type,
-		 * title, enum] [freifunk.community.location.lat] : [title, type,
-		 * description, required] [freifunk.community.techDetails.networks.ipv6]
-		 * : [type, title, description, required, items]
-		 * [freifunk.community.techDetails.networks.ipv4.items.network] : [type,
-		 * description, pattern, required] [freifunk.community.nodeMaps.items. :
-		 * [url, interval, technicalType, mapType]
-		 * [freifunk.community.location.address.Street] : [title, type,
-		 * description, required] [freifunk.community.techDetails.firmware.docs]
-		 * : [title, type, pattern, description, required]
-		 * [freifunk.community.contact.identica] : [title, type, pattern,
-		 * description, required] [freifunk.community.contact.phone] : [title,
-		 * type, description, required]
-		 * [freifunk.community.techDetails.networks.ipv4.items] : []
-		 * [freifunk.community.techDetails.dns.items.nameserver] : [type, title,
-		 * description, required, items] [freifunk.community.location] : []
-		 * [freifunk.community.techDetails.firmware. : [url, docs, name]
-		 * [freifunk.community.timeline.items. : [decription, timestamp]
-		 * [freifunk.community.services.items.externalUri] : [title, type,
-		 * description, required]
-		 * [freifunk.community.techDetails.dns.items.domainname] : [type,
-		 * description, pattern, required, title] [freifunk.community.nodeMaps]
-		 * : [type, description, required, items]
-		 * [freifunk.community.techDetails.routing] : [title, description,
-		 * required, type, items] [freifunk.community.nodeMaps.items] : []
-		 * [freifunk.community.techDetails.networks.ipv6.items] : []
-		 * [freifunk.community] : [api, name, metacommunity, location, contact,
-		 * url, timeline, feeds, state, nodeMaps, services, techDetails]
-		 * [freifunk.community.location.address. : [Name, Street, Zipcode]
-		 * [freifunk.community.timeline.items.decription] : [title, type,
-		 * description, required] [freifunk.community.state.lastchange] :
-		 * [title, type, description, default, required]
-		 * [freifunk.community.feeds] : [type, description, required, items]
-		 * [freifunk.community.techDetails. : [firmware, dns, networks, routing,
-		 * legals, updatemode] [freifunk.community.location.city] : [title,
-		 * type, description, required] [freifunk.community.contact] : []
-		 * [freifunk.community.techDetails.dns] : [title, descripition, type,
-		 * items] [freifunk.community.techDetails.legals] : [title, description,
-		 * required, type, items] [freifunk.community.contact.irc] : [title,
-		 * type, pattern, description, required]
-		 * [freifunk.community.techDetails.networks.ipv4] : [type, description,
-		 * required, items] [freifunk.community.url] : [title, type, pattern,
-		 * description, required] [freifunk.community.feeds.items.category] :
-		 * [title, type, enum, default, description, required]
-		 * [freifunk.community.services.items.internalUri] : [title, type,
-		 * description, required] [freifunk.community.metacommunity] : [title,
-		 * type, description, required] [freifunk.community.feeds.items.url] :
-		 * [title, type, description, required]
-		 * [freifunk.community.contact.jabber] : [title, type, pattern,
-		 * description, required] [freifunk.community.timeline.items.timestamp]
-		 * : [title, type, description, required]
-		 * [freifunk.community.state.description] : [title, type, description,
-		 * required] [freifunk.community.location.address] : []
-		 * [freifunk.community.contact.webform] : [title, type, description,
-		 * pattern, required] [freifunk.community.timeline] : [type,
-		 * description, required, items] [freifunk.community.nodeMaps.items.url]
-		 * : [title, type, description, required]
-		 * [freifunk.community.techDetails.updatemode] : [title, type,
-		 * description, required, items] [freifunk.community.state] : []
-		 * [freifunk.community.location.lon] : [title, type, description,
-		 * required] [freifunk.community.services.items.serviceDescription] :
-		 * [title, type, description, required]
-		 * [freifunk.community.techDetails.dns.items. : [domainname, nameserver]
-		 * [freifunk.community.techDetails.networks.ipv4.items. : [network]
-		 * [freifunk.community.feeds.items.name] : [title, type, description,
-		 * required]
-		 * [freifunk.community.techDetails.networks.ipv6.items.network] : [type,
-		 * description, pattern, required]
-		 * [freifunk.community.techDetails.legals.items] : [type, title, enum]
-		 * [freifunk.community.techDetails.firmware.name] : [title, type,
-		 * description, required] [freifunk.community.techDetails] : []
-		 * [freifunk.community.techDetails.networks.ipv6.items. : [network]
-		 * [freifunk.community.techDetails.dns.items] : []
-		 * [freifunk.community.name] : [title, type, description, required]
-		 * [freifunk.community.timeline.items] : []
-		 * [freifunk.community.services] : [type, description, required, items]
-		 * [freifunk.community.techDetails.networks. : [ipv6, ipv4]
-		 * [freifunk.community.nodeMaps.items.mapType] : [title, type,
-		 * description, enum, default, required]
-		 * [freifunk.community.state.nodes] : [title, type, description,
-		 * required] [freifunk.community.services.items. : [serviceName,
-		 * serviceDescription, externalUri, internalUri]
-		 * [freifunk.community.techDetails.dns.items.nameserver.items] : [type,
-		 * pattern, required] [freifunk.community.contact.email] : [title, type,
-		 * pattern, description, required]
-		 * [freifunk.community.nodeMaps.items.interval] : [title, type,
-		 * description, required] [freifunk.community.location. : [city,
-		 * country, address, lat, lon] [freifunk.community.contact.googleplus] :
-		 * [title, type, description, pattern, required]
-		 * [freifunk.community.services.items.serviceName] : [title, type,
-		 * description, required] [freifunk.community.state. : [nodes,
-		 * lastchange, message, description, focus]
-		 * [freifunk.community.feeds.items] : []
-		 * [freifunk.community.feeds.items. : [name, category, type, url]
-		 * [freifunk.community.contact.ml] : [title, type, description, pattern,
-		 * required] [start] : [schema] [freifunk.community.contact.facebook] :
-		 * [title, type, pattern, description, required]
-		 * [freifunk.community.state.message] : [title, type, description,
-		 * required] [freifunk.community.techDetails.routing.items] : [type,
-		 * title, enum] [freifunk.community.techDetails.firmware.url] : [title,
-		 * type, pattern, description, required]
-		 * [freifunk.community.location.address.Zipcode] : [title, type,
-		 * description, required] [start.schema] : []
-		 * [freifunk.community.location.country] : [title, type, description,
-		 * enum, default, required] [freifunk.community.techDetails.firmware] :
-		 * [] [freifunk.community.services.items] : []
-		 * [freifunk.community.contact. : [email, facebook, identica, irc,
-		 * jabber, ml, phone, googleplus, twitter, webform
-		 */
+
 	}
 }
